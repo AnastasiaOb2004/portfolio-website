f4afe73b0b9510f2b538743415d530b35a3a764a
AnastasiaObiezdova <138204188+AnastasiaObiezdova@users.noreply.github.com>
2025-03-18 23:35:55 +0100

    Import picture and create about page

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,7 +3,7 @@ const navbar = document.querySelector('#navbar');
 navbar.innerHTML += `
             <ul>
             <li><a href="index.html">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="about.html">About</a></li>
             <li><a href="skills.html">Skills</a></li>
             <li><a href="#work">Projects</a></li>
             <li><img src = "../images/icons/navbar-moon.svg" alt = "Moon icon"></li>
