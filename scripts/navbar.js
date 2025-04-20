bf9d60439ffa2da2cb3568923305761f454d453c
AnastasiaOb2004 <anobiezdova@gmail.com>
2025-04-20 22:21:53 +0200

    Update navbar.js with absolute paths

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,11 @@
 const navbar = document.querySelector('#navbar');
 
 navbar.innerHTML += `
-            <ul>
-            <li><a href="../index.html">Home</a></li>
-            <li><a href="../html/about.html">About</a></li>
-            <li><a href="../html/skills.html">Skills</a></li>
-            <li><a href="../html/projects.html">Projects</a></li>
-            <li><img src = "../images/icons/navbar-moon.png" alt = "Moon icon"></li>
-        </ul>
-`
+    <ul>
+        <li><a href="/portfolio-website/index.html">Home</a></li>
+        <li><a href="/portfolio-website/html/about.html">About</a></li>
+        <li><a href="/portfolio-website/html/skills.html">Skills</a></li>
+        <li><a href="/portfolio-website/html/projects.html">Projects</a></li>
+        <li><img src="/portfolio-website/images/icons/navbar-moon.png" alt="Moon icon"></li>
+    </ul>
+`;
