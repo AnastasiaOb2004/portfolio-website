96a34bd6401657f746cbe63d937e2b44a5da29e7
AnastasiaOb2004 <anobiezdova@gmail.com>
2025-04-21 11:17:11 +0200

    Update navbar.js, add burger menu and hidden menu

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,36 @@
-const navbar = document.querySelector('#navbar');
+function handleNavbarDisplay() {
+    const burgerMenu = document.querySelector('#burger-menu');
+    const hiddenMenu = document.querySelector('#hidden-menu');
+    const navbar = document.querySelector('#navbar');
 
-navbar.innerHTML += `
+    burgerMenu.style.display = 'none';
+    hiddenMenu.innerHTML = '';
+    navbar.innerHTML = '';
+
+    if (window.innerWidth <= 768) {
+        burgerMenu.style.display = 'flex';
+        let isClicked = false;
+        burgerMenu.addEventListener('click', () => {
+            hiddenMenu.style.transition = `0.5s ease-in-out`;
+            if (!isClicked) {
+                hiddenMenu.style.right = `10px`;
+                isClicked = true;
+            } else {
+                hiddenMenu.style.right = `-300px`;
+                isClicked = false;
+            }
+        });
+        hiddenMenu.innerHTML = `
+        <ul>
+        <li><a href="/portfolio-website/index.html">Home</a></li>
+        <li><a href="/portfolio-website/html/about.html">About</a></li>
+        <li><a href="/portfolio-website/html/skills.html">Skills</a></li>
+        <li><a href="/portfolio-website/html/projects.html">Projects</a></li>
+        </ul>
+      `;
+    } else {
+        navbar.style.display = `flex`;
+       navbar.innerHTML += `
     <ul>
         <li><a href="/portfolio-website/index.html">Home</a></li>
         <li><a href="/portfolio-website/html/about.html">About</a></li>
@@ -9,3 +39,8 @@ navbar.innerHTML += `
         <li><img src="/portfolio-website/images/icons/navbar-moon.png" alt="Moon icon"></li>
     </ul>
 `;
+    }
+}
+
+window.addEventListener('load', handleNavbarDisplay);
+window.addEventListener('resize', handleNavbarDisplay);
