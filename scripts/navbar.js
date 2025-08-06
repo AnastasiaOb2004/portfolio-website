a3068b92d937bcb96708a777cae7edf38a27249a
AnastasiaOb2004 <anobiezdova@gmail.com>
2025-08-06 10:44:37 +0200

    Fix contect of the pages

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -23,21 +23,20 @@ function handleNavbarDisplay() {
         });
         hiddenMenu.innerHTML = `
         <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="html/about.html">About</a></li>
-          <li><a href="html/skills.html">Skills</a></li>
-          <li><a href="html/projects.html">Projects</a></li>
+          <li><a href="/index.html">Home</a></li>
+          <li><a href="/html/about.html">About</a></li>
+          <li><a href="/html/skills.html">Skills</a></li>
+          <li><a href="/html/projects.html">Projects</a></li>
         </ul>
       `;
     } else {
         navbar.style.display = `flex`;
         navbar.innerHTML = `
         <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="html/about.html">About</a></li>
-          <li><a href="html/skills.html">Skills</a></li>
-          <li><a href="html/projects.html">Projects</a></li>
-          <li><img src="images/icons/navbar-moon.svg" alt="Moon icon"></li>
+          <li><a href="/index.html">Home</a></li>
+          <li><a href="/html/about.html">About</a></li>
+          <li><a href="/html/skills.html">Skills</a></li>
+          <li><a href="/html/projects.html">Projects</a></li>
         </ul>
       `;
     }
